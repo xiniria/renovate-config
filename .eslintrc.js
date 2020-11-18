e0f6ee140bc9db396818fded6ac61c54647bf391
Raphaël Dhôte <raphael.dhote@gmail.com>
2020-11-18 09:14:00 +0100

    added Airbnb ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: "module",
+    sourceType: 'module',
   },
   env: {
     node: true,
   },
   root: true,
+  plugins: [
+    'eslint-plugin-import',
+  ],
   extends: [
     'eslint:recommended',
+    'eslint-config-airbnb-base',
   ],
   rules: {
     semi: 'error',
