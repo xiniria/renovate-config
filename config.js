2089d57ade022af189ada437ff3007281249fc8d
Raphaël Dhôte <raphael.dhote@gmail.com>
2020-11-19 09:51:55 +0100

    added GitHub public token for docs-fetching in PRs

--- config.js
+++ config.js
@@ -5,6 +5,11 @@ module.exports = {
   endpoint: 'https://gitlab.illuin.tech/api/v4',
   token: process.env.RENOVATE_TOKEN,
 
+  hostRules: [{
+    domainName: 'github.com',
+    token: process.env.GITHUB_PUBLIC_TOKEN,
+  }],
+
   repositories: [
     '3ACentrale/2020-air-monitoring-pwa/renovate-config',
     '3ACentrale/2020-air-monitoring-pwa/air-monitoring-backend',
